b745c294a9e1adc661d0a4fd14f7d73b756b127f
zaimeali <zaimeali1997@gmail.com>
2022-02-14 17:18:18 -0500

    Hello world GET API added

--- StarterApp/views.py
+++ StarterApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+
+
+def hello_world(request):
+    return JsonResponse({
+        "message": "Hello World"
+    }, status=200)
